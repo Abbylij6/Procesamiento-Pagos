40338ffb3409bfbc51d49e1c1b00cb5cb6c68548
Abby <abbylijtestin@gmail.com>
2026-06-11 15:44:28 -0300

    se crea program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    Console.WriteLine();
+Console.WriteLine("===== SISTEMA DE PAGOS =====");
+
+List<Pago> pagos = new List<Pago>();
+
+pagos.Add(new TarjetaCredito("Juan Pérez", 25000));
+pagos.Add(new Transferencia("Ana López", 18000));
+
+foreach (Pago pago in pagos)
+{
+    pago.MostrarInfo();
+    pago.ProcesarPago();
+
+    Console.WriteLine("--------------------------------");
+}
